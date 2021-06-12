5b35a420adb6909dfdc734d3b284b04a969a741f
Angel Moreta <37674133+AngelM512@users.noreply.github.com>
2021-06-12 14:37:53 -0500

    Add files via upload

--- implementation.py
+++ implementation.py
@@ -1,5 +1,6 @@
 from decouple import config
 import tweepy
+import time
 #todo: create a file with their names + loop through em + search + get task done + delete file
 
 # Configuration stuff
@@ -10,20 +11,14 @@ ACCESS_TOKEN_ONE = config('ACCESS_TOKEN_ONE')
 ACCESS_TOKEN_TWO = config('ACCESS_TOKEN_TWO')
 ###############################################
 
-# GLOBAL API object
-
-
-
-
 # Authenticate to Twitter API
-def O_Auth():
+def API_PLUS_O_AUTH():
 
     # here goes the consumer key and secret
-    auth = tweepy.OAuthHandler(CONSUMER_KEY,
-    SECRET_KEY)
-    auth.set_access_token(ACCESS_TOKEN_ONE,
-    ACCESS_TOKEN_TWO)
+    auth = tweepy.OAuthHandler(CONSUMER_KEY,SECRET_KEY)
+    auth.set_access_token(ACCESS_TOKEN_ONE, ACCESS_TOKEN_TWO)
 
+    #API object
     API = tweepy.API(auth, wait_on_rate_limit=True,
                        wait_on_rate_limit_notify=True)
 
@@ -33,38 +28,59 @@ def O_Auth():
         print("\t\n [*][*][*] Authentication went through succesfully [*][*][*]\n")
     except tweepy.TweepError:
         print('Error! Failed to get request token.')
-    #fetching user
-    #user_id = 'elonmusk'
-    #user = api.get_user(user_id)
-    #print("user ID: ", user.id_str)
+
     return API
 
 
-def get_twitter_user_id():
-    api = O_Auth()
+def get_twitter_user_data():
+
+    #call func that gets twitter API ready
+    api = API_PLUS_O_AUTH()
+
     #ASK USER FOR TWITTER NAME
-    user_id = input("ENTER TWITTER NAME: ").lower()
+    user_id = input("ENTER TWITTER NAME: ").lower().replace(" ", "")
 
     if user_id[0] == '@':
         user_id = user_id.replace("@","")
 
-    ID = api.get_user(user_id)
+    # get me the id of the user provided + its most recent data
+    user_data = api.get_user(user_id)
+
+    #what to do when user is not found?
+    while True:
+        try:
+            print("User was found")
+            time.sleep(2)
+        except tweepy.TweepError as error:
+            print("\n\nUser not found, try again")
+            if error[0]['code'] == 50 or error[0]['message'] == 'User not found.':
+                user_id = input("ENTER TWITTER NAME: ").lower().replace(" ", "")
+                ID = api.get_user(user_id)
+                pass
+        break
+
+    return user_data #returns the whole Json file with user's info
 
 
-    return ID.id_str
-    #timeline = api.user_timeline(user_id)
-    #api.update_status(user_id + " hello")
+def get_latest_reply_id_and_message(data):
+    print(data)
+    #get tweet ID
+    tweet_id = data.status.in_reply_to_status_id
+    print("[*****]tweet_id = ", tweet_id, " data type= ", type(tweet_id))
+
+    #get latest message posted
+    latest_message = data.status.text
+
+    #store data and return
+    message_plus_tweetId = {
+                            'tweet_id': tweet_id,
+                            'message': latest_message
+                            }
+
+    return message_plus_tweetId
 
 
-
-
-
-
-def reply(user_id):
+def reply(data):
+    #data.update_status()
+    pass
     #create a tweet
-    pass
-
-
-
-def retweet(user_id, count=1):
-    pass
--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from implementation import *
 
 #main program function
 def main():
-
-    # get the user twitter id user you want to mimick
+    # get the user twitter id user you want to mimic
     # user must be provided like: @user123
-    id=get_twitter_user_id()
-    print(id)
+    data = get_twitter_user_data()
+    print("\n\n\n\n\n")
+    print('get_lattest_reply= ',get_latest_reply_id_and_message(data))
+
     #reply users latest tweet
     #reply()
 
